692c05ead2e2526717d31948c722eaa4830d58d2
Marcos Solis S <mss@192.168.1.18>
2022-08-11 04:04:10 -0500

    Last Update

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-const { request, response } = require('express');
 const express = require('express');
+const Contenedor = require('./src/fileSystem.js');
 
 const app = express();
+const cont = new Contenedor('./src/productos.txt');
 
-app.get('/', (request, response) => {
-    response.send('Servidor iniciado con express!');
+const port = 8080;
+
+app.get('/productos', async (request, response) => {
+    const products = await cont.getAll();
+
+    response.send(products);
 });
 
-app.get('/home', (request, response) => {
-    response.send('HOME!');
+app.get('/productoRandom', async (request, response) => {
+    const products = await cont.getAll();
+    const randomID = Math.floor(Math.random() * (Math.floor(products.length) - Math.ceil(1) + 1) + Math.ceil(1));
+    console.log('products.length',products.length);
+    console.log('randomID',randomID);
+    const randomProduct = await cont.getById(randomID);
+
+    response.send(randomProduct);
 });
 
 app.get('*', (request, response) => {
     response.send('404 - Page not found!!');
 });
 
-const server = app.listen(3000, () => {
-    console.log('Servidor http escuchando en http://localhost:3000/');
+const server = app.listen(port, () => {
+    console.log(`Servidor http escuchando en http://localhost:${port}/`);
 })
---
+++ src/filesystem.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+
+class Contenedor {
+    constructor(path) {
+        this.path = path
+    }
+
+    // Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
+    // Incorporará al producto un id numérico, que deberá ser siempre uno más que el id del último objeto agregado (o id 1 si es el primer objeto que se agrega) y no puede estar repetido.
+    async save(object) {
+        try {
+            const objects = await this.getAll();
+            let newID;
+    
+            if (objects[objects.length-1]?.id && objects[objects.length-1].id !== '') {
+                newID = objects[objects.length-1].id+1;
+            } else {
+                newID = 1;
+            }
+    
+            const newObj = {...object, id:newID};
+            objects.push(newObj);
+            await fs.promises.writeFile(this.path, JSON.stringify(objects, null, 2));
+
+            return newID;
+        } catch (error) {
+            console.log('Error al guardar: ',error)
+        }
+    }
+    
+    // Recibe un id y devuelve el objeto con ese id, o null si no está.
+    async getById(id) {
+        try {
+            const objects = await this.getAll();
+            const filteredObj = objects.filter(obj => obj.id === id)
+            return filteredObj != '' ? filteredObj : "Product doesn't found"
+        } catch (error) {
+            console.log('Error getById: ',error)
+        }
+    }
+    
+    // Devuelve un array con los objetos presentes en el archivo.
+    async getAll() {
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf8');
+            return JSON.parse(data);
+        } catch (error) {
+            console.log('Error al leer el archivo');
+            return [];
+        }
+    }
+
+    // Elimina del archivo el objeto con el id buscado.
+    async deleteById(id) {
+        try {
+            const objects = await this.getAll();
+            const filteredObj = objects.filter(obj => obj.id !== id);
+            filteredObj !== '' ? await fs.promises.writeFile(this.path, JSON.stringify(filteredObj, null, 2)) : console.log('deleteById(id): ', 'id doesn\'t found.');
+        } catch (error) {
+            console.log('deleteById(id): ', error);
+        }
+    }
+
+    // Elimina todos los objetos presentes en el archivo.
+    async deleteAll() {
+        try {
+            await fs.promises.writeFile(this.path, JSON.stringify([], null, 2))
+        } catch (error) {
+            console.log('deleteAll(): ', error);
+        }
+    }
+}
+
+module.exports = Contenedor;
